5e8b777f2d897aaea946df32472ad865ade99210
BaconErie <77812176+BaconErie@users.noreply.github.com>
2021-06-22 23:50:21 +0000

    Added server handling of remove and changed some client function names

--- main.py
+++ main.py
@@ -14,22 +14,42 @@ def todo(listID):
 @app.route("/<listID>/api", methods = ["GET", "POST"])
 def api(listID):
   if request.method == "POST":
-    global todoDatabase
-    #First get the data
+    #Get the JSON
     requestTable = request.get_json()
-    requestData = requestTable["data"]
-    #Then put into database
+    #Check what the client wants to do
+    if requestTable["type"] == "add":
+      #Add task
 
-    #Check if there is already a list
-    if listID in todoDatabase.keys():
-      #If there is, just add it to the list
-      todoDatabase[listID].append(requestData)
-    else:
-      #Otherwise, create a new list and add data to list
-      todoDatabase[listID] = []
-      todoDatabase[listID].append(requestData)
-    print(todoDatabase[listID])
-    return "Successfully POSTed data"
+      global todoDatabase
+      #First get the data
+      requestData = requestTable["data"]
+      #Then put into database
+
+      #Check if there is already a list
+      if listID in todoDatabase.keys():
+        #If there is, just add it to the list
+        todoDatabase[listID].append(requestData)
+      else:
+        #Otherwise, create a new list and add data to list
+        todoDatabase[listID] = []
+        todoDatabase[listID].append(requestData)
+      print(todoDatabase[listID])
+      return "Successfully added task"
+
+    if requestTable["type"] == "remove":
+      #Remove Task
+
+      #Check if list exists
+      global todoDatabase
+      if listID in todoDatabase.keys():
+        #Check if item exists
+        if requestTable["item"] <= len(todoDatabase[listID]) and requestTable["item"] > 0:
+          #If all condtions are true, remove data from table
+          todoDatabase[listID].pop(requestTable["item"])
+        else:
+          return "Item not in list"
+      else:
+        return "List does not exists"
 
   elif request.method == "GET":
     #Check if there is data for the to do list
