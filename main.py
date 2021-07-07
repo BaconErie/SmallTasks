761890dd8d4e3a42e91e4142719bc32c13043adc
BaconErie <77812176+BaconErie@users.noreply.github.com>
2021-07-07 21:55:58 +0000

    In development cross session saving. Check the other branches

--- main.py
+++ main.py
@@ -1,4 +1,58 @@
 from flask import Flask, render_template, request
+import sqlite3
+
+######################### Data Saving
+
+#Tasks Table Creation
+def createTable():
+  connection = sqlite3.connect("tasksData.db")
+  cursor = connection.cursor()
+
+  #Check if table exists
+  cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='Tasks';")
+  connection.commit()
+  output = cursor.fetchone()
+  
+  if not output:
+    #If the table doesn't exist, create table
+    cursor.execute("CREATE TABLE Tasks (listID int, taskNum int, task text)")
+
+    connection.commit()
+  
+  connection.close()
+
+#Get Todo List data from Tasks table
+def getList(listID):
+  connection = sqlite3.connect("tasksData.db")
+  cursor = connection.cursor()
+  print(listID)
+  cursor.execute("SELECT * FROM Tasks WHERE listID=? ORDER BY taskNum", [listID])
+  connection.commit()
+  output = cursor.fetchone()
+
+  connection.close()
+
+  return output
+
+def setList(listID, todoList):
+  connection = sqlite3.connect("tasksData.db")
+  cursor = connection.cursor()
+
+  #First delete all current entries that belong to the todoList
+  cursor.execute("DELETE FROM Tasks WHERE listID=?", [listID])
+  connection.commit()
+
+  #Then loop through the given todo list and enter all the new entries into the table
+  for x in range(len(todoList)):
+    cursor.execute("INSERT INTO Tasks (listID, taskNum, task) VALUES (?, ?, ?)", (listID, x + 1, todoList[x]))
+    connection.commit()
+  
+  connection.close()
+
+#Run the createTable function on startup
+createTable()
+
+########################### End Data Saving Code
 
 app = Flask(__name__)
 todoDatabase = {}
@@ -22,28 +76,41 @@ def api(listID):
       #Add task
 
       global todoDatabase
-      #First get the data
+      #Get the data that is to be added to the todo list
       requestData = requestTable["data"]
-      #Then put into database
+
+      #Edit list and then put list into the database
+
+      currentList = getList(listID)
 
       #Check if there is already a list
-      if listID in todoDatabase.keys():
-        #If there is, just add it to the list
-        todoDatabase[listID].append(requestData)
+      if currentList:
+        #If there is, just add to the list and push to database
+        currentList.append(requestData)
+        setList(listID, currentList)
+        return "Added data success"
+
       else:
         #Otherwise, create a new list and add data to list
-        todoDatabase[listID] = []
-        todoDatabase[listID].append(requestData)
+        currentList = []
+        currentList.append(requestData)
+        #Push data to database
+        setList(listID, currentList)
+        return "Created list and added data success"
 
     if requestTable["type"] == "remove":
       #Remove Task
 
+      currentList = getList(listID)
+
       #Check if list exists
-      if listID in todoDatabase.keys():
+      if currentList:
         #Check if item exists
-        if int(requestTable["item"]) <= len(todoDatabase[listID]) and int(requestTable["item"]) > 0:
+        if int(requestTable["item"]) <= len(currentList) and int(requestTable["item"]) > 0:
           #If all condtions are true, remove data from table
-          todoDatabase[listID].pop(int(requestTable["item"]) - 1)
+          currentList.pop(int(requestTable["item"]) - 1)
+          #Push data to database
+          setList(listID, currentList)
         else:
           return "Item not in list"
       else:
@@ -51,15 +118,20 @@ def api(listID):
 
   elif request.method == "GET":
     #Check if there is data for the to do list
-    if listID in todoDatabase.keys():
+    
+    currentList = getList(listID)
+    
+    if currentList:
       #If there is data send data
-      responseDict = {"data": todoDatabase[listID]}
+      responseDict = {"data": currentList}
       return responseDict
     else:
       #If there isn't return nothing
       return {"data": "nothing"}
   
   return "Success"
+  
+
 
 if __name__ == "__main__":
   app.run(debug=True, host="0.0.0.0")
