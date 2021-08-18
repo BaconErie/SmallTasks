769097cb9093ae77cce4ce5cabbf963a1c9772cc
BaconErie <77812176+BaconErie@users.noreply.github.com>
2021-08-17 21:17:15 -0400

    Fixes to list.py
    - Changed cursor.commit() to connection.commit()
    - Edited remove_item() so that the task_num of tasks are updated and not messed up.

--- list.py
+++ list.py
@@ -7,7 +7,7 @@ def check_list_permission(function):
         cursor = connection.cursor()
 
         cursor.execute('SELECT * FROM list WHERE user_id=? AND list_id=?', (user_id, list_id))
-        cursor.commit()
+        connection.commit()
 
         if len(cursor.fetchone()) == 0:
             connection.close()
@@ -19,12 +19,12 @@ def check_list_permission(function):
     return action
 
 @check_list_permission
-def add_item(user_id, list_id, item_num, task_body):
+def add_item(user_id, list_id, task_body):
     connection = sqlite3.connect('SmallTasks_Data.db')
     cursor = connection.cursor()
 
-    cursor.execute('INSERT INTO task (task_body, list_id, item_num) VALUES (?, ?, ?)', (task_body, list_id, item_num))
-    cursor.commit()
+    cursor.execute('INSERT INTO task (task_body, list_id) VALUES (?, ?)', (task_body, list_id))
+    connection.commit()
 
     connection.close()
 
@@ -32,9 +32,18 @@ def add_item(user_id, list_id, item_num, task_body):
 def remove_item(user_id, list_id, item_num):
     connection = sqlite3.connect('SmallTasks_Data.db')
     cursor = connection.cursor()
-
-    cursor.execute('DELETE FROM task WHERE list_id=? AND item_num=?', (list_id, item_num))
-    cursor.commit()
+    
+    #Get current list contents to be added later
+    contents = get_list(user_id, list_id)
+    contents.pop(item_num - 1)
+    
+    #Delete all tasks part of list
+    cursor.execute('DELETE FROM task WHERE list_id=?', (list_id)
+    connection.commit()
+    
+    #Add tasks back in table except for deleted task
+    for task in contents:
+        add_item(user_id, list_id, task)
 
     connection.close()  
 
@@ -49,14 +58,14 @@ def create_list(user_id):
         new_list_id = secrets.token_urlsafe(8)
 
         cursor.execute('SELECT * FROM list WHERE list_id=?', (new_list_id))
-        cursor.commit()
+        connection.commit()
 
-        if len(cursor.fetchone()) == 0:
+        if len(cursor.fetchall()) == 0:
             #Unique id
             not_generated = False
 
     cursor.execute('INSERT INTO list (list_id, user_id) VALUES (?, ?)', (new_list_id, user_id))
-    cursor.commit()
+    connection.commit()
 
     connection.close()
 
@@ -67,7 +76,7 @@ def get_list(user_id, list_id):
     cursor = connection.cursor()
 
     cursor.execute('SELECT task_body FROM task WHERE list_id=? ORDER BY task_num', (list_id))
-    cursor.commit()
+    connection.commit()
 
     output = cursor.fetchall()
 
